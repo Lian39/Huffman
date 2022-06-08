1764d32458b19c1a47d501d9a3f7676426fdd273
Lian39 <dreamsella4@gmail.com>
2022-06-08 12:57:56 +0300

    createCodesTable function addded

--- Huffman/Huffman.cpp
+++ Huffman/Huffman.cpp
@@ -6,14 +6,24 @@
 #include <queue>
 #include <map>
 
-void buildFreqsTable(std::ifstream &file, std::multimap<int, char> &freqsTable)
+void readFile(std::string &path, std::string &data)
+{
+    std::ifstream inputFile(path, std::ifstream::binary);
+    
+    while (!inputFile.eof())
+    {
+        char ch;
+        inputFile.read(&ch, sizeof(ch));
+        data += ch;
+    }
+}
+
+void createFreqsTable(std::string &data, std::multimap<int, char> &freqsTable)
 {
     std::vector<int> freqs(0x100);
 
-    while (!file.eof())
+    for (auto ch : data)
     {
-        char ch;
-        file.read(&ch, sizeof(ch));
         freqs[static_cast<unsigned char>(ch)]++;
     }
 
@@ -24,9 +34,8 @@ void buildFreqsTable(std::ifstream &file, std::multimap<int, char> &freqsTable)
     }
 }
 
-void buildTree(std::multimap<int, char> &freqsTable)
+void buildTree(std::priority_queue<Node *, std::vector<Node *>, comp> &pq, std::multimap<int, char> &freqsTable)
 {
-    std::priority_queue<Node *, std::vector<Node *>, comp> pq;
 
     for (auto pair : freqsTable)
     {
@@ -45,8 +54,26 @@ void buildTree(std::multimap<int, char> &freqsTable)
         Node *newNode = new Node('\0', freqsSum, left, right);
         pq.push(newNode);
     }
+}
 
-    std::cout << pq.top()->freq << "\n";
+void createCodesTable(Node *node, std::string code, std::multimap<char, std::string> &codesTable)
+{
+    if (node == nullptr)
+        return;
+
+    if (!node->left && !node->right)
+        codesTable.insert(std::make_pair(node->ch, code));
+
+    createCodesTable(node->left, code + "0", codesTable);
+    createCodesTable(node->right, code + "1", codesTable);
+}
+
+void encode(std::string &data, std::multimap<char, std::string> &codesTable, std::vector<bool> &compressedData)
+{
+    for (auto ch : data)
+    {
+        
+    }
 }
 
 int main(int argv, char *argc[])
@@ -92,15 +119,23 @@ int main(int argv, char *argc[])
                 inputFiles.push_back(std::string(argc[i]));
         }
 
-        std::multimap<char, std::string> codesTable;
-        inputFiles.push_back("./test.txt");
         for (size_t i = 0; i < inputFiles.size(); ++i)
         {
-            std::ifstream file(inputFiles[i], std::ios::binary);
-            std::multimap<int, char> freqsTable;
+            std::string data;
+            readFile(inputFiles[i], data);
 
-            buildFreqsTable(file, freqsTable);
-            buildTree(freqsTable);
+            std::multimap<int, char> freqsTable;
+            createFreqsTable(data, freqsTable);
+
+            std::priority_queue<Node *, std::vector<Node *>, comp> pq;
+            buildTree(pq, freqsTable);
+            Node *root = pq.top();
+
+            std::multimap<char, std::string> codesTable;
+            createCodesTable(root, "", codesTable);
+
+            std::vector<bool> compressedData;
+            encode(data, codesTable, compressedData);
         }
     }
     return 0;
